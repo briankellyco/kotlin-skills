222b140a86720091d5b2ca5096d387fbc124aae2
briankellyco <7138538+briankellyco@users.noreply.github.com>
2024-02-09 15:18:39 +0100

    sandbox-unpublished init

---
+++ sandbox/src/main/kotlin/co/bk/kotlinskills/sandbox/AnonymousObjects.kt
@@ -0,0 +1,37 @@
+package co.bk.kotlinskills.sandbox
+
+fun main(args: Array<String>) {
+
+    // Create an anonymous object using the "object" keyword
+
+    /*
+     * Expression body example
+     */
+    wantsSomeInterface(object: AnonymousObjectsInterface {
+        // we want an expression body here not a block body e.g inline declaration
+        override fun mustImplement(num: Int) =
+            "this is from mustImplement: ${num * 10}"
+    })
+
+
+
+//    /*
+//     * Block body example
+//     */
+//    var thisIsMutable = 45
+//    wantsSomeInterface(object: AnonymousObjectsInterface {
+//        // block body style with brackets and return
+//        override fun mustImplement(num: Int): String {
+//            thisIsMutable++ // var accessible from anonymous object.... not possible in java
+//            return "this is from mustImplement: ${num * 10}"
+//      }
+//    })
+}
+
+interface AnonymousObjectsInterface {
+    fun mustImplement(num: Int): String
+}
+
+fun wantsSomeInterface(si: AnonymousObjectsInterface) {
+    println("printing from wantsSomeInterface ${si.mustImplement(22)}")
+}
